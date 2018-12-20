b7e93d205b3865733d9fc0fc18b062315936b656
Bruce <ilywanzi@gmail.com>
2018-12-19 22:12:44 -0800

    Updates

---
+++ precache-manifest.9053c58299f7c2ec5eb830faa4392711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ffc04bdf822354594af",
+    "url": "/React---Redux-Image-Search/static/js/runtime~main.8ffc04bd.js"
+  },
+  {
+    "revision": "3663348e803bbe2ed929",
+    "url": "/React---Redux-Image-Search/static/js/main.3663348e.chunk.js"
+  },
+  {
+    "revision": "6c75f5874ebcdbfa8355",
+    "url": "/React---Redux-Image-Search/static/js/1.6c75f587.chunk.js"
+  },
+  {
+    "revision": "3663348e803bbe2ed929",
+    "url": "/React---Redux-Image-Search/static/css/main.12218ef7.chunk.css"
+  },
+  {
+    "revision": "783313f3c9f9746a02406fbcfa582a28",
+    "url": "/React---Redux-Image-Search/index.html"
+  }
+];
